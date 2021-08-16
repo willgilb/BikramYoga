705362676dc00ab47f303250795c67313a861f4b
svanlaere <svanlaere@users.noreply.github.com>
2021-08-16 23:01:05 +0200

    Update User.php

--- app/Model/User.php
+++ app/Model/User.php
@@ -52,4 +52,13 @@ class User
         $stmt->execute();
         return $stmt->fetch();
     }
+
+    public function byEmail(string $email)
+    {
+        $sql = "SELECT * FROM users WHERE email = :email";
+        $stmt = $this->db->prepare($sql);
+        $stmt->bindValue(':email', $email, \PDO::PARAM_STR);
+        $stmt->execute();
+        return $stmt->fetch();
+    }
 }
