140c59d3349daa65e88bd691015459b850cf4204
svanlaere <svanlaere@users.noreply.github.com>
2021-08-16 22:45:02 +0200

    Create register_success.php

---
+++ app/View/register_success.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>Thanks for registering</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="<?php echo CSS_PATH; ?>form.css">
+</head>
+
+<body>
+    <div class="wrapper">
+        <div class="login-card">
+            <header class="header">
+                <h1>Thanks for registering</h1>
+            </header>
+            <main>
+                <p>You have succesfully registered. You can now <a href="<?php echo URI_PUBLIC; ?>/login">Login</a>.</p>
+            </main>
+        </div>
+    </div>
+</body>
+
+</html>
