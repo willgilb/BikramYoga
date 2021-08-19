78d44438fcf2f1fdb1c54b1f9436e1bd66688800
svanlaere <svanlaere@users.noreply.github.com>
2021-08-19 18:38:08 +0200

    Update db.sql

--- install/db.sql
+++ install/db.sql
@@ -1,9 +1,23 @@
-CREATE TABLE IF NOT EXISTS `user_foo` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` tinyint(4) NOT NULL AUTO_INCREMENT,
   `username` varchar(50) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `email` varchar(50) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `role_id` tinyint(4) DEFAULT NULL,
   `pwhash` char(60) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `email` (`email`),
   UNIQUE KEY `username` (`username`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+CREATE TABLE IF NOT EXISTS `roles` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `role` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `perms` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `role` (`role`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+
+INSERT INTO `roles` (`id`, `role`, `perms`) VALUES
+	(1, 'manager', '{\r\n   "can_add":1,\r\n   "can_delete":1,\r\n   "can_edit":1,\r\n   "can_view":1\r\n}'),
+	(2, 'teacher', '{\r\n   "can_add":0,\r\n   "can_delete":0,\r\n   "can_edit":1,\r\n   "can_view":1\r\n}'),
+	(3, 'student', '{\r\n   "can_add":0,\r\n   "can_delete":0,\r\n   "can_edit":0,\r\n   "can_view":1\r\n}');
